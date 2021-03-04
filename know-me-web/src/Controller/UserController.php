561bc5546c43ff99d26a7361b5b7eb575828049b
wiem benabada <wiem.benabada@esprit.tn>
2021-03-04 22:32:19 +0100

    cm

--- know-me-web/src/Controller/UserController.php
+++ know-me-web/src/Controller/UserController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 class UserController extends AbstractController
 {
     /**
